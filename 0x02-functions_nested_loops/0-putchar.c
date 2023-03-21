91373ab87b5028023aef582b222c17c3e6530c5f
ANSAH <kinoansah@gmail.com>
2023-03-21 07:43:25 -0700

    readme

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - Entry point
+ * main - prints robert to stdout
  *
- * Return: Always 0 (Success)
+ * Return: 0 if no error
  */
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
-
