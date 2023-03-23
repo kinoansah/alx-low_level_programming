31fac577bdcce157a7cb9c2295906d7e63cf39a6
ANSAH <kinoansah@gmail.com>
2023-03-23 05:49:12 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - checks for uppercase character
+ * @c: character that is checked
+ * is written by sirking
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+		return (0);
+}
