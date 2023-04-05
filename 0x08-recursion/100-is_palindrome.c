e22818c843850e32891896a1c4d5dd1f66eaacca
ANSAH <kinoansah@gmail.com>
2023-04-05 06:24:50 -0700

    A fraction of our brains' potential

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,57 @@
 #include "main.h"
 
-int check_pal(char *s, int i, int len);
-int _strlen_recursion(char *s);
-
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to reverse
- *
- * Return: 1 if it is, 0 if it's not
+ * get_length - Entry point
+ * Description: Get the legth
+ * @s: Character
+ * Return: 1 if palindrome 0 otherwise
  */
-int is_palindrome(char *s)
-{
-	if (*s == 0)
-		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
-}
 
-/**
- * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
- *
- * Return: length of the string
- */
-int _strlen_recursion(char *s)
+int get_length(char *s)
 {
 	if (*s == '\0')
+	{
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	}
+	return (1 + get_length(s + 1));
 }
 
 /**
- * check_pal - checks the characters recursively for palindrome
- * @s: string to check
- * @i: iterator
- * @len: length of the string
- *
- * Return: 1 if palindrome, 0 if not
+ * check_palindrome - Entry point
+ * Description: Checking Palindrome
+ * @s: Character
+ * @start: Integer
+ * @end: Integer
+ * Return: 1 if palindrome 0 otherwise
  */
-int check_pal(char *s, int i, int len)
+
+int check_palindrome(char *s, int start, int end)
 {
-	if (*(s + 1) != *(s + len - 1))
-		return (0);
-	if (i >= len)
+	if (start >= end)
+	{
 		return (1);
-	return (check_pal(s, i + 1, len - 1));
+	}
+	if (*(s + start) == *(s + end))
+	{
+		return (check_palindrome(s, start + 1, end - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - Entry point
+ * Description: Palindrome
+ * @s: Character
+ * Return: 1 if palindrome 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int len = get_length(s);
+
+	if (len == 0 || len == 1)
+	{
+		return (1);
+	}
+	return (check_palindrome(s, 0, len - 1));
 }
