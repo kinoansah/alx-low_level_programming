807c34b97f1b99e356b42a24df19ff9c6abf39c1
ANSAH <kinoansah@gmail.com>
2023-04-08 11:34:32 -0700

    modified 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of strings containing the arguments passed to the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-    int i;
+int i;
 
-    for (i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
-    }
+    /* Iterate over each argument and print it on a new line */
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
 
-    return 0;
+    /* Return 0 to indicate successful completion of the program */
+return (0);
 }
