64638e6345211dbeed7593306d73a1a8aa511b51
ANSAH <kinoansah@gmail.com>
2023-04-13 03:35:03 -0700

    We must accept finite disappointment, but never lose infinite hope

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * multiply - multiplies two integers
+ * @num1: the first integer to multiply
+ * @num2: the second integer to multiply
+ *
+ * Return: the product of num1 and num2
+ */
+int multiply(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * main - multiplies two positive numbers passed as command-line arguments
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments
+ *
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = multiply(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
