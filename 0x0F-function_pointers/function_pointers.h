747f761782dc6da6c8670fc66aa6a8f7ef9196e4
ANSAH <kinoansah@gmail.com>
2023-04-19 10:51:27 -0700

    What's my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef Main_h
+#define Main_h
+
+#include <stdlib.h>
+#include <stddef.h>
 
 int _putchar(char c);
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
 int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
