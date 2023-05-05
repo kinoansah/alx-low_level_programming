c8146d2468342ebb5eb71a3f357a2731efccfc81
kinoansah <kinoansah@gmail.com>
2023-05-05 08:26:49 -0700

    Correction for Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,31 +1,18 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation of a number.
- * @n: unsigned long int to be converted to binary and printed.
+ * print_binary - prints the binary representation of an unsigned long int
+ * @n: the number to print in binary
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
-	int bit_count = 0;
-
-	/* Find the most significant bit in n */
-	while ((mask << 1) <= n)
+	if (n > 1)
 	{
-		mask <<= 1;
-		bit_count++;
+		print_binary(n >> 1);
 	}
-
-	/* Print the binary representation of n */
-	while (mask > 0)
-	{
-		if ((n & mask) == mask)
-			_putchar('1');
-		else
-			_putchar('0');
-		mask >>= 1;
-	}
-	if (bit_count == 0)
-		_putchar('0');
+	_putchar((n & 1) + '0');
 }
 
