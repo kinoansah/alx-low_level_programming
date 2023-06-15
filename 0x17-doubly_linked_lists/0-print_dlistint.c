a552943047fa8dfae66e083cfff0f46308a89fd8
kinoansah <kinoansah@gmail.com>
2023-06-15 05:20:15 -0700

    Print list with betty corrections

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints all the elements of a doubly linked list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
@@ -12,5 +18,5 @@ size_t print_dlistint(const dlistint_t *h)
 		count++;
 	}
 
-	return count;
+	return (count);
 }
