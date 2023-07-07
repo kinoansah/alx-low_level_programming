c6a62bd928f33cc41063c622940580844c84830e
kinoansah <kinoansah@gmail.com>
2023-07-07 08:56:31 -0700

    Corrections on Task 3 and header file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,6 +18,7 @@ hash_node_t *create_hash_node(const char *key, const char *value)
 		return (NULL);
 
 	new_node->key = strdup(key);
+
 	if (new_node->key == NULL)
 	{
 		free(new_node);
@@ -53,7 +54,6 @@ int update_hash_node_value(hash_node_t *node, const char *value)
 
 	free(node->value);
 	node->value = new_value;
-
 	return (1);
 }
 
@@ -88,8 +88,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node == NULL)
 		return (0);
 
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new_node;
+	}
+	else
+	{
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
+	}
 
 	return (1);
 }
